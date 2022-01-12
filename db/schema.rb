2af87bbfce2e0b53d41806213dd3a34d9a4609fe
M20190141187 <lucasvictor222@ufrn.edu.br>
2022-01-12 14:57:53 -0300

    teste

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_11_215407) do
+ActiveRecord::Schema.define(version: 2022_01_12_175728) do
+
+  create_table "clientes", force: :cascade do |t|
+    t.string "name"
+    t.date "datadenascimento"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "homes", force: :cascade do |t|
     t.string "index"
